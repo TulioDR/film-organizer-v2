83e8baf74a6bf208bd9cf7e0c6700331a0910d62
Tulio <tulioruzo29@gmail.com>
2022-12-01 21:49:12 -0400

    Update Upcoming movies with release date

--- src/components/home/upcoming/Upcoming.tsx
+++ src/components/home/upcoming/Upcoming.tsx
@@ -1,4 +1,9 @@
 import { useRouter } from "next/router";
+import {
+   changeDateFormat,
+   daysToRelease,
+   isReleased,
+} from "../../../utils/date";
 import Poster from "../../Poster";
 
 type Props = { movie: any };
@@ -20,8 +25,18 @@ export default function Upcoming({ movie }: Props) {
             size="xl"
             backPoster
          />
-         <div className="w-full absolute bottom-0 bg-gradient-to-t from-gray-900 to-transparent px-4 pb-3 pt-10">
-            <div className="text-lg">{movie.title}</div>
+         <div className="w-full absolute bottom-0 bg-gradient-to-t from-gray-900 to-transparent px-4 pb-3 pt-14">
+            <div className="">{movie.title}</div>
+            <div className="flex space-x-2 text-gray-500 dark:text-gray-300 text-sm">
+               <span>{changeDateFormat(movie.release_date)}</span>
+               {isReleased(movie.release_date) ? (
+                  <span className="ml-1">(released)</span>
+               ) : (
+                  <span className="ml-1">
+                     (in {daysToRelease(movie.release_date)} days)
+                  </span>
+               )}
+            </div>
          </div>
       </div>
    );
