4f957b8840dba887cf337e85d5c32b0b2c5b29ca
Tulio <tulioruzo29@gmail.com>
2023-01-24 19:34:42 -0400

    Update styles on Mobile Sidebar

--- src/layout/sidebar/SidebarContainer.tsx
+++ src/layout/sidebar/SidebarContainer.tsx
@@ -13,7 +13,7 @@ export default function SidebarContainer({ children }: Props) {
          }`}
       >
          {/* The change on the padding right on hover occurs because of the scrollbar width */}
-         <div className="w-full h-full py-5 pr-5 lg:pr-2 hover:lg:pr-0 overflow-y-hidden hover:overflow-y-scroll sidebar-scrollbar space-y-2">
+         <div className="w-full h-full py-5 pr-5 hover:pr-3 lg:pr-2 hover:lg:pr-0 overflow-y-hidden hover:overflow-y-scroll sidebar-scrollbar space-y-2">
             {children}
          </div>
       </div>
