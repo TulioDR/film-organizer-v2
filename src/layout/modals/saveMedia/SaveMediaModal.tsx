5a7625a0d56750065e434c4f7c8498f1ea4df315
Tulio <tulioruzo29@gmail.com>
2023-01-25 14:33:47 -0400

    Update SaveMediaModal with message if there is no lists

--- src/layout/modals/saveMedia/SaveMediaModal.tsx
+++ src/layout/modals/saveMedia/SaveMediaModal.tsx
@@ -18,16 +18,24 @@ export default function SaveMediaModal() {
    return (
       <ModalContainer isOpen={isSaveMediaOpen} close={closeSaveMediaModal}>
          <ModalTitle>Save to...</ModalTitle>
-         <ul className="w-56 border-y border-light-text-soft dark:border-dark-text-soft">
-            {lists.map((list) => (
-               <ListToSave
-                  key={list.id}
-                  list={list}
-                  media={currentMedia}
-                  mediaType={currentType}
-               />
-            ))}
-         </ul>
+         <div className="w-56 border-y border-light-text-soft dark:border-dark-text-soft">
+            {lists.length > 0 ? (
+               lists.map((list) => (
+                  <ListToSave
+                     key={list.id}
+                     list={list}
+                     media={currentMedia}
+                     mediaType={currentType}
+                  />
+               ))
+            ) : (
+               <div className="text-sm text-center my-5">
+                  You need to create Lists first to start saving Movies and TV
+                  Series in them
+               </div>
+            )}
+         </div>
+
          <ModalButtonsContainer>
             <ModalButton onClick={closeSaveMediaModal}>Close</ModalButton>
          </ModalButtonsContainer>
