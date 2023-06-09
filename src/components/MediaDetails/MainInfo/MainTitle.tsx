bc93655dac7a984d257fe128be2521084116247f
Tulio <tulioruzo29@gmail.com>
2023-06-08 23:48:02 -0400

    Update Media Details initial animations

--- src/components/MediaDetails/MainInfo/MainTitle.tsx
+++ src/components/MediaDetails/MainInfo/MainTitle.tsx
@@ -1,15 +1,11 @@
-import RevealHorizontal from "@/animations/RevealHorizontal";
-
 type Props = {
    children: React.ReactNode;
 };
 
 export default function MainTitle({ children }: Props) {
    return (
-      <RevealHorizontal>
-         <h1 className="text-6xl xl:text-7xl 2xl:text-8xl font-medium font-oswald leading-tight">
-            {children}
-         </h1>
-      </RevealHorizontal>
+      <h1 className="text-6xl xl:text-7xl 2xl:text-8xl font-medium font-oswald leading-tight">
+         {children}
+      </h1>
    );
 }
--- src/components/MediaDetails/MainInfo/index.tsx
+++ src/components/MediaDetails/MainInfo/index.tsx
@@ -5,6 +5,10 @@ import Rating from "./Rating";
 import Runtime from "./Runtime";
 import Score from "./Score";
 import MainBookmark from "../MainBookmark";
+import { motion } from "framer-motion";
+
+import RevealHorizontal from "@/animations/RevealHorizontal";
+import { staggerContainer } from "@/animations/StaggerCards";
 
 type Props = {
    media: any;
@@ -26,31 +30,48 @@ export default function MainInfo({ media, mediaType }: Props) {
       ));
    };
    return (
-      <div className="flex-1 overflow-hidden h-full flex flex-col gap-5 justify-center pl-10">
-         <MainTitle>{media.title || media.name}</MainTitle>
-         <div className="flex items-end gap-2">
-            <Date date={media.first_air_date || media.release_date} />
-            <span className="text-xl">○</span>
-            <Rating
-               rating={
-                  media.release_dates?.results || media.content_ratings?.results
-               }
-               isMovie={mediaType === "movie"}
-            />
-         </div>
-         <div className="flex items-center gap-2 text-sm">
-            {mediaType == "movie" && (
-               <>
-                  <Runtime runtime={media.runtime} />
+      <div className="flex-1 overflow-hidden h-full pl-10">
+         <motion.div
+            variants={staggerContainer}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="w-full h-full flex flex-col gap-5 justify-center overflow-hidden"
+         >
+            <RevealHorizontal stagger>
+               <MainTitle>{media.title || media.name}</MainTitle>
+            </RevealHorizontal>
+            <RevealHorizontal stagger>
+               <div className="flex items-end gap-2">
+                  <Date date={media.first_air_date || media.release_date} />
                   <span className="text-xl">○</span>
-               </>
-            )}
-            <span>{separateArray(media.genres)}</span>
-         </div>
-         <div className="flex items-center gap-5">
-            <Score score={media.vote_average} />
-            <MainBookmark media={media} mediaType={mediaType} />
-         </div>
+                  <Rating
+                     rating={
+                        media.release_dates?.results ||
+                        media.content_ratings?.results
+                     }
+                     isMovie={mediaType === "movie"}
+                  />
+               </div>
+            </RevealHorizontal>
+            <RevealHorizontal stagger>
+               <div className="flex items-center gap-2 text-sm">
+                  {mediaType == "movie" && (
+                     <>
+                        <Runtime runtime={media.runtime} />
+                        <span className="text-xl">○</span>
+                     </>
+                  )}
+                  <span>{separateArray(media.genres)}</span>
+               </div>
+            </RevealHorizontal>
+            <RevealHorizontal stagger>
+               <div className="flex items-center gap-5">
+                  <Score score={media.vote_average} />
+                  <MainBookmark media={media} mediaType={mediaType} />
+               </div>
+            </RevealHorizontal>
+         </motion.div>
       </div>
    );
 }
