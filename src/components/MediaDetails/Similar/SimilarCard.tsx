68eafd7324e931555b91c0b8234b8c81081d7ac1
Tulio <tulioruzo29@gmail.com>
2023-06-17 15:45:03 -0400

    Update Similar Card animation poster update

--- src/components/MediaDetails/Similar/SimilarCard.tsx
+++ src/components/MediaDetails/Similar/SimilarCard.tsx
@@ -1,6 +1,8 @@
 import Poster from "@/components/Poster";
 import { MediaModel } from "@/models/MediaModel";
+import { posterAnimationActions } from "@/store/slices/poster-animation-slice";
 import Link from "next/link";
+import { useDispatch } from "react-redux";
 
 type Props = {
    similar: MediaModel;
@@ -8,8 +10,10 @@ type Props = {
 };
 
 export default function SimilarCard({ similar, href }: Props) {
+   const dispatch = useDispatch();
    const handleClick = () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
+      dispatch(posterAnimationActions.changePosterAnimation(true));
    };
 
    return (
