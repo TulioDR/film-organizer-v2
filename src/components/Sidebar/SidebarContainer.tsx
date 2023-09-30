b3daf16f969d76c8817477ddf4522b0c7484a4e6
Tulio <tulioruzo29@gmail.com>
2023-09-30 18:03:00 -0400

    Update Sidebar Container

--- src/components/Sidebar/SidebarContainer.tsx
+++ src/components/Sidebar/SidebarContainer.tsx
@@ -13,24 +13,26 @@ export default function SidebarContainer({ children }: Props) {
    );
    return (
       <div
+         id="sidebar"
          className={`fixed lg:sticky top-0 z-40 self-start h-screen duration-300 ${
-            expandSidebar ? "w-60" : "w-[120px]"
-         } ${revealSidebar ? "" : "-translate-x-full lg:translate-x-0"}`}
+            revealSidebar ? "" : "-translate-x-full lg:translate-x-0"
+         }`}
       >
-         <div
-            id="sidebar"
-            className="h-full bg-primary-light dark:bg-primary-dark"
+         <motion.div
+            variants={staggerContainer}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="h-full bg-primary-light dark:bg-primary-dark p-10 hover:pr-8 overflow-y-hidden hover:overflow-y-scroll main-scrollbar"
          >
-            <motion.div
-               variants={staggerContainer}
-               initial="initial"
-               animate="animate"
-               exit="exit"
-               className="w-full h-full p-10 hover:pr-8 overflow-y-hidden overflow-x-hidden hover:overflow-y-scroll main-scrollbar space-y-5"
+            <div
+               className={`${
+                  expandSidebar ? "w-40" : "w-10"
+               } duration-300 space-y-5`}
             >
                {children}
-            </motion.div>
-         </div>
+            </div>
+         </motion.div>
       </div>
    );
 }
