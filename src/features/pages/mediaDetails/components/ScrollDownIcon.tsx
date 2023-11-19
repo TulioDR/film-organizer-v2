f4f3c6bcf1e3200f316d642d5c11abc767ae0819
Tulio <tulioruzo29@gmail.com>
2023-11-19 18:07:41 -0400

    Update ScrollDownIcon

--- src/features/pages/mediaDetails/components/ScrollDownIcon.tsx
+++ src/features/pages/mediaDetails/components/ScrollDownIcon.tsx
@@ -1,7 +1,9 @@
+import { motion } from "framer-motion";
+
 export default function ScrollDownIcon() {
    const ScrollIcon = () => {
       return (
-         <div className="flex flex-col items-center ">
+         <div className="flex flex-col items-center">
             <div className="border border-light-1 dark:border-dark-1 sm:border-dark-1 h-14 w-8 rounded-xl pt-3">
                <div className="mx-auto animate-bounce bg-light-1 dark:bg-dark-1 sm:bg-dark-1 w-1 h-4 rounded-full"></div>
             </div>
@@ -14,12 +16,24 @@ export default function ScrollDownIcon() {
 
    return (
       <>
-         <div className="absolute bottom-0 right-0 hidden sm:block">
+         <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="absolute bottom-0 right-0 hidden sm:block"
+         >
             <ScrollIcon />
-         </div>
-         <div className="sm:hidden absolute top-5 left-1/2 -translate-x-1/2 bg-primary-light dark:bg-primary-dark rounded-2xl p-3 pb-1">
+         </motion.div>
+         <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="sm:hidden absolute top-5 left-1/2 -translate-x-1/2 bg-primary-light dark:bg-primary-dark rounded-2xl p-3 pb-1"
+         >
             <ScrollIcon />
-         </div>
+         </motion.div>
       </>
    );
 }
