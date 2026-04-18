b52c4fca2612b73c8a2ea3771c9203002f58f2c2
Tulio <tulioruzo29@gmail.com>
2026-04-18 16:18:24 -0400

    Update learn more button on the card so that it looks like the one on the card

--- src/features/media-card/components/Back/LearnMore.tsx
+++ src/features/media-card/components/Back/LearnMore.tsx
@@ -10,9 +10,12 @@ export default function LearnMore({ onClick, id }: Props) {
       <motion.button
          layoutId={`back-loading-${id}`}
          onClick={onClick}
-         className="text-white dark:text-black bg-accent flex items-center justify-center flex-1 h-full rounded-md"
+         className={`text-black dark:text-white border-accent border-2 flex items-center justify-center flex-1 h-full rounded-md
+            hover:bg-black hover:text-white dark:hover:text-black dark:hover:bg-white
+            active:bg-black active:text-white dark:active:text-black dark:active:bg-white
+         `}
       >
-         <span className="text-xs 2xl:text-sm font-medium uppercase">
+         <span className="text-xs 2xl:text-sm font-light uppercase">
             Learn More
          </span>
       </motion.button>
