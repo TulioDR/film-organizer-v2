15350b5d5265e767eaa0a975e4864aa1d0144818
Tulio <tulioruzo29@gmail.com>
2026-04-18 16:25:59 -0400

    Update loader

--- src/features/layout/loader/components/PageLoader/LoadingBoard.tsx
+++ src/features/layout/loader/components/PageLoader/LoadingBoard.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import Clapperboard from "../Clapperboard";
 
 type Props = {};
 
 export default function LoadingBoard({}: Props) {
    return (
-      <div className="h-screen w-full fixed top-0 left-0 z-[200]">
-         <div className="absolute right-4 bottom-4 flex items-center justify-center">
+      <div className="h-[100svh] w-full fixed top-0 left-0 z-[200]">
+         <div className="absolute right-4 bottom-16 translate-y-1 xl:translate-y-0 xl:bottom-4 flex items-center justify-center">
             <div className="w-56 rounded-md aspect-square bg-white dark:bg-black shadow-md border border-border-light dark:border-border-dark overflow-hidden">
                <div className="w-full h-full flex flex-col gap-2 items-center justify-center translate-y-[5%]">
                   <div className="w-3/6 -rotate-[10deg] origin-bottom-left">
--- src/features/layout/loader/components/PageLoader/index.tsx
+++ src/features/layout/loader/components/PageLoader/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import useAppSelector from "@/store/hooks/useAppSelector";
 import useAppDispatch from "@/store/hooks/useAppDispatch";
