161cdde2da8cb2e03edf1c1f53d02ba1a25bf42d
Tulio <tulioruzo29@gmail.com>
2026-04-23 07:35:05 -0400

    SL 2

---
+++ src/pages/sso-callback.tsx
@@ -0,0 +1,23 @@
+import { useClerk } from "@clerk/nextjs";
+import { useEffect } from "react";
+
+export default function SSOCallback() {
+   const { handleRedirectCallback } = useClerk();
+
+   useEffect(() => {
+      const handleCallback = async () => {
+         await handleRedirectCallback({
+            signInFallbackRedirectUrl: "/",
+            signUpFallbackRedirectUrl: "/",
+         });
+      };
+
+      handleCallback();
+   }, [handleRedirectCallback]);
+
+   return (
+      <div className="h-[100svh] w-full flex items-center justify-center">
+         <div>Loading...</div>
+      </div>
+   );
+}
